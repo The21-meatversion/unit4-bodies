105914580a847cf0b85af766f0929f1b2543953d
The21-meatversion <>
2025-07-01 13:03:49 +0700

    uploading

---
+++ script.js
@@ -0,0 +1,77 @@
+function face() {
+    const audio = document.getElementById('face');
+    audio.play();
+}
+
+function hand() {
+    const audio = document.getElementById('hand');
+    audio.play();
+}
+
+function ear() {
+    const audio = document.getElementById('ear');
+    audio.play();
+}
+
+function eye() {
+    const audio = document.getElementById('eye');
+    audio.play();
+}
+
+function showhideText() {
+    const text = document.getElementById("eng-word");
+    const btn = document.getElementById("showhide-btn");
+    if (text.style.display === "none") {
+        text.style.display = "block";
+        btn.innerHTML = "🙈"; // Mắt gạch
+    } else {
+        text.style.display = "none";
+        btn.innerHTML = "👁️"; // Mắt mở
+    }
+}
+
+function showhideHand() {
+    const text = document.getElementById("hand-en");
+    const btn = document.getElementById("showhideHand-btn");
+    if (text.style.display === "none") {
+        text.style.display = "block";
+        btn.innerHTML = "🙈"; // Mắt gạch
+    } else {
+        text.style.display = "none";
+        btn.innerHTML = "👁️"; // Mắt mở
+    }
+}
+
+function showhideEar() {
+    const text = document.getElementById("ear-en");
+    const btn = document.getElementById("showhideEar-btn");
+    if (text.style.display === "none") {
+        text.style.display = "block";
+        btn.innerHTML = "🙈"; // Mắt gạch
+    } else {
+        text.style.display = "none";
+        btn.innerHTML = "👁️"; // Mắt mở
+    }
+}
+
+function showhideEye() {
+    const text = document.getElementById("eye-en");
+    const btn = document.getElementById("showhideEye-btn");
+    if (text.style.display === "none") {
+        text.style.display = "block";
+        btn.innerHTML = "🙈"; // Mắt gạch
+    } else {
+        text.style.display = "none";
+        btn.innerHTML = "👁️"; // Mắt mở
+    }
+}
+
+function toggleFullScreen() {
+  if (!document.fullscreenElement) {
+    document.documentElement.requestFullscreen();
+  } else {
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    }
+  }
+}
